18ce4ff5cd6dc28c7227988a35c8b1d472a4813b
noob1200 <kanaria2411@gmail.com>
2023-09-07 07:13:38 +0900

    Webアプリケーションの実装3

--- src/chatbot_engine.py
+++ src/chatbot_engine.py
@@ -1,15 +1,21 @@
 import langchain
 from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import DirectoryLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain.memory import ChatMessageHistory
-from langchain.schema import HumanMessage
 
 langchain.verbose = True
 
 
-def chat(message: str, history: ChatMessageHistory) -> str:
+def create_index() -> VectorStoreIndexWrapper:
+    loader = DirectoryLoader("./src/", glob="**/*.py")
+    return VectorstoreIndexCreator().from_loaders([loader])
+
+
+def chat(
+    message: str, history: ChatMessageHistory, index: VectorStoreIndexWrapper
+) -> str:
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
-    messages = history.messages
-    messages.append(HumanMessage(content=message))
-
-    return llm(messages).content
+    return index.query(question=message, llm=llm)
